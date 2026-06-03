23fa2c54c40714ffc621abe371420008d1177b51
DEVANSHEE CHODVADIYA <devanshee.chodvadiya@gmail.com>
2026-06-03 15:13:32 +0530

    util code discusion

--- src/index.js
+++ src/index.js
@@ -6,24 +6,37 @@ dotenv.config({
     path: './env'
 })
 
-// import express from "express"
-// const app = express()
-// ( async() => {
-//     try {
-//         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error",(error)=> {
-//             console.error("ERROR: ",error)
-//             throw err
-//         })
 
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port ${process.env.PORT}`);
-//         })
-//     }
-//     catch (error){
-//         console.error("ERROR: ",error)
-//         throw err
-//     }
-// })()
+
 
 connectDB()
+.then(() => {
+    app.listen(process.env. port || 8000, () => {
+        console.log(`Server is running at port : $
+        {process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+})
+
+
+/*import express from "express"
+const app = express()
+( async() => {
+    try {
+        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=> {
+            console.error("ERROR: ",error)
+            throw err
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }
+    catch (error){
+        console.error("ERROR: ",error)
+        throw err
+    }
+})()*/
