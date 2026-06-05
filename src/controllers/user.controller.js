d78f45a3924d65ddcfc4e8297212f819dc6e150e
DEVANSHEE CHODVADIYA <devanshee.chodvadiya@gmail.com>
2026-06-05 15:13:03 +0530

    file uploads

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,7 +24,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -32,8 +32,8 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(409, "User with email or username already exists")
     }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.
     path;
 
     if(!avatarLocalPath){
