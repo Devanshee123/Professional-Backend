6290cfc8db6bccc29e29e03f252f569d793a9654
DEVANSHEE CHODVADIYA <devanshee.chodvadiya@gmail.com>
2026-06-06 16:20:27 +0530

    file upload done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,68 +2,68 @@ import mongoose, {Schema} from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
-
 const userSchema = new Schema(
     {
-        username:{
+        username: {
             type: String,
             required: true,
             unique: true,
             lowercase: true,
-            trim: true,
+            trim: true, 
             index: true
         },
-         email:{
+        email: {
             type: String,
             required: true,
             unique: true,
             lowercase: true,
-            trim: true,
+            trim: true, 
         },
-         fullName:{
+        fullName: {
             type: String,
             required: true,
-            trim: true,
+            trim: true, 
             index: true
         },
-         avatar:{
+        avatar: {
             type: String, // cloudinary url
             required: true,
         },
-         coverImage:{
+        coverImage: {
             type: String, // cloudinary url
         },
-         watchhistory:{
-            type: Schema.Types.ObjectId,
-            ref: "Video"
-        },
-         password:{
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
             type: String,
             required: [true, 'Password is required']
         },
-         refreshToken:{
-            type: String,
+        refreshToken: {
+            type: String
         }
+
     },
     {
-        timestamps: true,
+        timestamps: true
     }
 )
 
-userSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function() {
+    if (!this.isModified("password")) return;
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
+    this.password = await bcrypt.hash(this.password, 10);
+});
 
-userSchema.methods.isPasswordCorrect = async function
-(password){
+userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
 userSchema.methods.generateAccessToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
             email: this.email,
@@ -74,19 +74,21 @@ userSchema.methods.generateAccessToken = function(){
         {
             expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
-   )
+    )
 }
 
+
 userSchema.methods.generateRefreshToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
+            
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
             expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
-   )
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,8 +17,8 @@ const uploadOnCLoudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         //file has been uploaded successfull
-        console.log("file is uploaded on cloudinary",
-        response.url);
+        //console.log("file is uploaded on cloudinary",response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     }
     catch(error){
